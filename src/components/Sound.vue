169f135c5fb4050fee90bd3d2f58d4e616f68889
skondratenko <skondratenko@asset-control.com>
2017-08-29 15:21:07 +0200

    Initial commit

---
+++ src/components/Sound.vue
@@ -0,0 +1,35 @@
+<template>
+  <button v-cloak="true" v-on:click="playPause()">{{getButtonName()}}</button>
+</template>
+<script>
+  import SoundService from './SoundService.js'
+
+  export default {
+    name : 'sound',
+    data() {
+      return {
+        isStarted : false,
+        sound: null
+      }
+    },
+    methods : {
+      getButtonName : function() {
+        return this.isStarted ? "Stop" : "Start";
+      },
+      playPause : function() {
+        if (!this.sound) {
+          this.sound = new SoundService();
+        }
+        this.isStarted = !this.isStarted;
+        if (this.isStarted) {
+          this.sound.play();
+        } else {
+          this.sound.stop();
+        }
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
